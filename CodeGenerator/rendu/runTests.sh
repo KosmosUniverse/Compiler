26adda14cbbeda0b64c47e7ac9564128061c49d7
thomas <thomas.ducret@epitech.eu>
2019-12-16 05:55:58 +0100

    add .ir file generation and refactor runTests.sh script

--- CodeGenerator/rendu/runTests.sh
+++ CodeGenerator/rendu/runTests.sh
@@ -4,6 +4,7 @@ rm -rf ../tests/output
 mkdir ../tests/output
 cp -r ../tests/input/* .
 for file in *.txt; do
-    java CodeGenerator $file > "../tests/output/output_$file.txt"
+    java CodeGenerator $file > "../tests/output/output_$file"
 done
+cp *.ir ../tests/output/
 rm *.txt
